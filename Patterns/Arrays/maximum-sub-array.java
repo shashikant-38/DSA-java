03b5c60e91df15880e642e2d8851b3c5b5efad2a
shashikant <patilshashikant4508@gmail.com>
2026-03-26 22:36:27 +0530

    Add maxSubArray method to find maximum subarray
    Implement Kadane's algorithm to find the maximum subarray sum.

--- Patterns/Arrays/maximum-sub-array.java
+++ Patterns/Arrays/maximum-sub-array.java
@@ -4,3 +4,23 @@
 // 2,Add current element
 // 3.Update answer
 // 4.If sum becomes negative → reset
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = nums[0];
+
+        for (int num : nums) {
+            sum += num;
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
