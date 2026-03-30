46e33e8f901ead94fedf3113ab85214f7a1330a4
shashikant <patilshashikant4508@gmail.com>
2026-03-31 01:39:56 +0530

    Implement twoSum method using HashMap

--- Patterns/Hashing/Two-Sum*.java
+++ Patterns/Hashing/Two-Sum*.java
@@ -1,7 +1,21 @@
 // store -> check ->done 
 //   store needed value and check remaining 
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1}; // not found
     }
 }
